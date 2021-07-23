251b5d976d78ca8c02ac1874e7346d684daf4b53
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-24 00:13:01 +0530

    Added Hello and tachyon in index.html and rendered Hello component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './hello';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Hello />,
   document.getElementById('root')
 );
 
