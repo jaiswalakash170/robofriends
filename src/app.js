8421b204c549f99979d3159352fa584c24950308
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-24 15:25:19 +0530

    Added constructor inside App to have state object and updated SearchBox component to take onSearchChange function as PROPS

--- src/app.js
+++ src/app.js
@@ -3,18 +3,25 @@ import CardList from './cardlist';
 import {robots} from './robots.js';
 import SearchBox from './searchbox';
 
-const state = {
-    robots: robots,
-    searchfield: ""
-};
-
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        };
+    }
+
+    onSearchChange(event){
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <div className="tc">
                 <h1>Robofriends</h1>
-                <SearchBox />
-                <CardList robots={robots}/>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots}/>
             </div>
         );
     }
