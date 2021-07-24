136c48fe02f8f4b7f270ff70d84a6b4dd868e83a
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-24 18:28:32 +0530

    Style Scroll component to make it scrollable component

--- src/scroll.js
+++ src/scroll.js
@@ -1,7 +1,11 @@
 import react from 'react';
 
 const Scroll = (props) => {
-    return props.children;
+    return (
+        <div style={{overflowY:'scroll', border:'5px solid black', height:'600px'}}>
+            {props.children}
+        </div>
+    );
 }
 
 export default Scroll;
