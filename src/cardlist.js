770eed36cd7b747b9736f85b30d8f3898ef89781
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-24 14:45:57 +0530

    Added key to each Card to make sure we can keep track which one to modify later if needed

--- src/cardlist.js
+++ src/cardlist.js
@@ -2,12 +2,19 @@ import react from 'react';
 import Card from './card';
 
 const CardList = ({robots}) => {
-    const cardComponent = robots.map((user, i) => {
-        return (<Card id={robots[i].id} name={robots[i].name} username={robots[i].username} email={robots[i].email} />);
-    });
     return (
     <div>
-        {cardComponent}
+        {
+            robots.map((user, i) => {
+                return (<Card 
+                            key={i} 
+                            id={robots[i].id} 
+                            name={robots[i].name} 
+                            username={robots[i].username} 
+                            email={robots[i].email} 
+                />);
+            })
+        }
     </div>
     );
 }
